24cda50e686d733cb57bb421a805672a350502d2
AlunoETC <alunoetc@EDUC.NET>
2025-10-09 20:13:50 -0300

    v10

--- paciente/cadastro.php
+++ paciente/cadastro.php
@@ -6,14 +6,14 @@ $nome = $_POST['nome'];
 $email = $_POST['email'];
 $senha = $_POST['senha'];
 $endereco = $_POST['endereco'];
-$cpf = $_POST['cpf'];   
+  
 
 
 // 3. (opcional mas recomendado) Criptografa a senha
 $senha_hash = password_hash($senha, PASSWORD_DEFAULT);
 
 // 4. Comando SQL para inserir os dados
-$sql = "INSERT INTO usuario (nome, email, senha, endereco, cpf) VALUES ('$nome','$email', '$senha_hash', '$endereco', '$cpf')";
+$sql = "INSERT INTO usuario (nome, email, senha, endereco) VALUES ('$nome','$email', '$senha_hash', '$endereco')";
 
 // 5. Executa o comando
 if ($conn->query($sql) === TRUE) {
@@ -22,8 +22,8 @@ if ($conn->query($sql) === TRUE) {
     echo "Erro: " . $sql . "<br>" . $conn->error;
 }
 
-$stmt = $conn->prepare("INSERT INTO usuario (nome, email, senha, endereco, cpf) VALUES (?, ?, ?, ?, ?)");
-$stmt->bind_param("sssss", $nome, $email, $senha_hash, $endereco);
+$stmt = $conn->prepare("INSERT INTO usuario (nome, email, senha, endereco) VALUES (?, ?, ?, ?)");
+$stmt->bind_param("ssss", $nome, $email, $senha_hash, $endereco);
 $stmt->execute();
 
 // 6. Fecha a conexão
